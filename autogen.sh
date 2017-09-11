f3365a209ecdb6dc66a781a9bb3fe9a99a78b433
Default User <laurence.a.maddox@gmail.com>
2017-09-11 09:56:21 -0500

    updated autogen.sh for separate build dir

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,10 @@
 set -exo nounset
 
 if [ $# -ne 0 ] ; then
+	git pull origin master
+	git submodule foreach git fetch
+	git submodule foreach git checkout master
+	git submodule foreach git merge origin/master
 	if [ -f Makefile ] ; then
 		make distclean
 	fi
@@ -12,7 +16,10 @@ autoreconf --install
 if [ $# -eq 0 ] ; then exit 0
 else
 	test "x$1" == xmake
-	./configure --prefix=$HOME
+	DIR="$(readlink -f `pwd`)"
+	mkdir ../CAQ-build
+	cd ../CAQ-build
+	"$DIR/configure" "--prefix=$HOME"
 	make
 	make install
 fi
